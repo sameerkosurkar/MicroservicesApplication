73fb4ae5a81014bdb46d5356546a1245487adf3e
NC24625_Sameer <nc24625_sameer@ninja-its-MacBook-Air-148.local>
2023-07-13 15:54:35 +0530

    Unit Test for SpecimenService layer added

--- src/main/java/com/example/MicroservicesApplication/dto/Specimen.java
+++ src/main/java/com/example/MicroservicesApplication/dto/Specimen.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class Specimen {
     private Integer plantId;
-    private String specimenId;
+    private Integer specimenId;
     private String latitude;
     private String longitude;
     private String description;
