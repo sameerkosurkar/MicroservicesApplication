b41acf714a6a18275e3bc3c479b88613ad73aa23
NC24625_Sameer <nc24625_sameer@ninja-its-MacBook-Air-148.local>
2023-07-17 15:23:25 +0530

    Frontend page

--- src/main/java/com/example/MicroservicesApplication/PlantDiaryController.java
+++ src/main/java/com/example/MicroservicesApplication/PlantDiaryController.java
@@ -6,39 +6,63 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
-@ResponseBody
 public class PlantDiaryController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        Specimen specimen = new Specimen();
+        specimen.setSpecimenId(69);
+        model.addAttribute(specimen);
+        return "index";
+    }
+
+    @RequestMapping("/sustainability")
+    public String sustainability() {
+        return "sustainability";
+    }
 
     @Autowired
     SpecimenService specimenService;
 
-    @GetMapping("/specimen/")
+    @RequestMapping("/saveSpecimen")
+    public String save(Specimen specimen) {
+        specimenService.save(specimen);
+        return "index";
+    }
+
+    @RequestMapping("/plants")
+    public ResponseEntity searchPlant(@RequestParam(value = "searchPlant", required = false) String searchPlant) {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/specimen")
     public ResponseEntity<List<Specimen>> fetchAll() {
         return new ResponseEntity<List<Specimen>>(specimenService.fetchAll(), HttpStatus.OK);
     }
 
-    @GetMapping("/specimen/{id}/")
+    @GetMapping("/specimen/{id}")
     public Specimen fetchById(@PathVariable("id") Integer id) {
         return specimenService.fetchById(id);
     }
 
-    @PostMapping(value = "/specimen/")
+    @PostMapping(value = "/specimen")
     public ResponseEntity<HttpStatus> saveSpecimen(@RequestBody Specimen specimen) {
         specimenService.save(specimen);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @PutMapping(value = "/specimen/{id}/")
+    @PutMapping(value = "/specimen/{id}")
     public void updateSpecimen(@PathVariable("id") Integer id, @RequestBody Specimen specimen) {
         specimenService.update(id, specimen);
     }
 
-    @DeleteMapping("/specimen/{id}/")
+    @DeleteMapping("/specimen/{id}")
     public void deleteSpecimen(@PathVariable("id") Integer id) {
         specimenService.delete(id);
     }
