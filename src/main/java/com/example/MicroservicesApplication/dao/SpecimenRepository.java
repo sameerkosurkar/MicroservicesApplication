3fa019a4c1a0242c30ff34df5718aaddf13e3a3b
NC24625_Sameer <nc24625_sameer@ninja-its-MacBook-Air-148.local>
2023-07-19 18:18:07 +0530

    Setting up environment for testing

--- src/main/java/com/example/MicroservicesApplication/dao/SpecimenRepository.java
+++ src/main/java/com/example/MicroservicesApplication/dao/SpecimenRepository.java
@@ -4,5 +4,4 @@ import com.example.MicroservicesApplication.dto.Specimen;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SpecimenRepository extends CrudRepository<Specimen, Integer> {
-
 }
