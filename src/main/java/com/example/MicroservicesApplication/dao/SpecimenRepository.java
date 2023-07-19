53f04d18cd5a1bbade139b3c6abbfe90391efde0
NC24625_Sameer <nc24625_sameer@ninja-its-MacBook-Air-148.local>
2023-07-19 18:46:59 +0530

    testing

--- src/main/java/com/example/MicroservicesApplication/dao/SpecimenRepository.java
+++ src/main/java/com/example/MicroservicesApplication/dao/SpecimenRepository.java
@@ -1,7 +1,9 @@
 package com.example.MicroservicesApplication.dao;
 
 import com.example.MicroservicesApplication.dto.Specimen;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.CrudRepository;
 
+@Profile("!test")
 public interface SpecimenRepository extends CrudRepository<Specimen, Integer> {
 }
