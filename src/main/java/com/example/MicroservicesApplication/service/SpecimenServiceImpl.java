a696c3a73db272cba0a483755dacfb5e67e74cb0
NC24625_Sameer <nc24625_sameer@ninja-its-MacBook-Air-148.local>
2023-07-21 16:03:06 +0530

    Implemented Caching

--- src/main/java/com/example/MicroservicesApplication/service/SpecimenServiceImpl.java
+++ src/main/java/com/example/MicroservicesApplication/service/SpecimenServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.MicroservicesApplication.dto.Specimen;
 import com.example.MicroservicesApplication.dao.PlantDAO;
 import com.example.MicroservicesApplication.dao.SpecimenDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -19,6 +21,7 @@ public class SpecimenServiceImpl implements SpecimenService{
     @Autowired
     PlantDAO plantDAO;
     @Override
+    @Cacheable(value = "specimen", key = "#id")
     public Specimen fetchById(int id) {
         return specimenDAO.fetchById(id);
     }
@@ -29,18 +32,20 @@ public class SpecimenServiceImpl implements SpecimenService{
     }
 
     @Override
+    @Cacheable("specimens")
     public List<Specimen> fetchAll() {
         return specimenDAO.fetchAll();
     }
 
     @Override
+    @CacheEvict(value = "specimen", key = "#id")
     public void delete(Integer id) {
         specimenDAO.delete(id);
     }
 
     @Override
+    @Cacheable("plants")
     public List<Plant> fetchPlants(String combinedName) throws IOException {
         return plantDAO.fetchPlants(combinedName);
     }
-
 }
